db10fbcbc619feaf39ca9eba850644b85cdc5b90
Ahmad Zaki Fitra <99023372+zachfitra@users.noreply.github.com>
2025-07-17 11:49:43 +0700

    menambahkan fitur crud di controller

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -22,3 +22,38 @@ export const getUserById = async(req, res) =>{
         console.log(error.message);
     }
 }
+
+export const createUser = async(req, res) =>{
+    try {
+        await User.create(req.body);
+        res.status(201).json({msg : "User Created"});
+    } catch (error){
+        console.log(error.message);
+    }
+}
+
+export const updateUser = async(req, res) =>{
+    try {
+        await User.update(req.body,{
+            where:{
+                id : req.params.id
+            }
+        });
+       res.status(200).json({msg : "User Updated"});
+    } catch (error){
+        console.log(error.message);
+    }
+}
+
+export const deleteUser = async(req, res) =>{
+    try {
+        await User.destroy({
+            where:{
+                id : req.params.id
+            }
+        });
+       res.status(200).json({msg : "User Deleted"});
+    } catch (error){
+        console.log(error.message);
+    }
+}
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,10 +1,18 @@
 import express from "express";
-import {getUserById, getUsers} from "../controller/UserController.js";
+import {getUserById, 
+        getUsers,
+        createUser,
+        updateUser,
+        deleteUser} 
+from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
+router.post('/users',createUser);
+router.patch('/users/:id',updateUser);
+router.delete('/users/:id',deleteUser);
 
 export default router;
 
