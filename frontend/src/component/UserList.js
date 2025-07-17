140b0871e31ae7b1a1a93af4dc1b22a11f1b293f
Ahmad Zaki Fitra <99023372+zachfitra@users.noreply.github.com>
2025-07-17 14:31:55 +0700

    menampilkan tampilan react

---
+++ frontend/src/component/UserList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserList = () => {
+  return (
+    <div className="columns mt-5 is-centered" >
+        <div className="column is-half">
+            ini adalah frontend
+        </div>
+    </div>
+  )
+}
+
+export default UserList
