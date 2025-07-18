09c357926b02272ec9fc6f81b7b9232ada316f64
Ahmad Zaki Fitra <99023372+zachfitra@users.noreply.github.com>
2025-07-18 15:32:38 +0700

    menampilkan data crud dan fitur add user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import UserList from "./component/UserList";
+import AddUser from "./component/AddUser";
 
 function App() {
   return (
-    <div>
-      <UserList/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<UserList/>}/>
+        <Route path = "/add" element = {<AddUser/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
